e0a997f1a4621a2faea651e34be82710b6be8ca5
Atlas_WebDev <web.dev89@yandex.by>
2022-11-05 17:46:58 +0300

    update

--- elastic-slave/init/start.sh
+++ elastic-slave/init/start.sh
@@ -6,6 +6,18 @@ if [[ -n $tz ]];
 then 
 	ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 fi
+
+/usr/share/elasticsearch/bin/elasticsearch-certutil cert --ca /usr/share/elasticsearch/config/cert/elastic-stack-ca.p12
+
+
+
+
+
+
+
+
+
+
 # Запуст команды из под пользователя elasticsearch
 su elasticsearch -c /usr/share/elasticsearch/bin/elasticsearch
 
