1a62470061e4661d4d527fb81a9d71253abe64e4
Atlas_WebDev <web.dev89@yandex.by>
2022-11-07 03:00:10 +0300

    update

---
+++ get-password/get-password.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# Скрипт для формирование паролей для встроенных пользователей в elastic
+# Полученные пароли сохраняются в файл и нужны для настройки kibana и других сервисов
+# Первым параметром передаем название одной из нод в cluster
+
+if ! [[ -z $1 ]]
+then
+    HOST=$1
+else 
+    echo "Not set name host"
+    exit 0
+fi
+
+/usr/bin/expect <<END
+    set timeout 120
+    spawn docker exec -it ${HOST} bash -c "cd bin/ && elasticsearch-setup-passwords auto"
+            expect {
+                    "*\]" { 
+                            log_file passwords.txt
+                            send -- "y\r" 
+                            expect "dfrc"
+                    }
+                } 
+END
+
+if [[ -f passwords.txt ]]
+then
+    cd ..  
+    rm -R elastic-stack-passwords 
+    mkdir elastic-stack-passwords
+    mv ${PWD}/get-password/passwords.txt elastic-stack-passwords
+    rm ${PWD}/get-password/passwords.txt
+else
+    echo "file password not found"
+    exit 1
+fi
+
+exit 0
