389e8ff431e333ea958dd37c63c38bad501f3825
Atlas_WebDev <web.dev89@yandex.by>
2022-11-15 01:03:55 +0300

    kibana

---
+++ kibana/test.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo -e "\n" >> /usr/share/kibana/config/kibana.yml
+echo "#elasticsearch.username:\n" >> /usr/share/kibana/config/kibana.yml
+echo "#elasticsearch.password:\n" >> /usr/share/kibana/config/kibana.yml
+exit 0
